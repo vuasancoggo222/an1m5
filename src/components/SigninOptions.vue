cba5b6793e5d35443c78eabc6b41ede73d640e0f
vulee <levu08072002@gmail.com>
2023-04-03 17:11:03 +0700

    test provider

--- src/components/SigninOptions.vue
+++ src/components/SigninOptions.vue
@@ -2,13 +2,15 @@
 import { GoogleAuthProvider,FacebookAuthProvider } from '@firebase/auth';
 import useSignInWithThirdParty from '@/uses/useSignInWithThirdParty';
 const {signInWithThirdParty} = useSignInWithThirdParty()
+const facebookProvider = new FacebookAuthProvider()
+const googleProvider = new GoogleAuthProvider()
 </script>
 
 <template>
  <div>
         <div class="mb-4">Or sign in with</div> 
-        <v-btn color="error" class="mx-2" @click="signInWithThirdParty(new GoogleAuthProvider())">Google</v-btn>
-        <v-btn color="primary" class="mx-2" @click="signInWithThirdParty(new FacebookAuthProvider())">Facebook</v-btn>   
+        <v-btn color="error" class="mx-2" @click="signInWithThirdParty(googleProvider)">Google</v-btn>
+        <v-btn color="primary" class="mx-2" @click="signInWithThirdParty(facebookProvider)">Facebook</v-btn>   
     </div>
 
 </template>
